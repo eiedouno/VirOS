77ddbd04d170bb2640a0f47fb36b41d15732e08f
GradyJSmith <grady.jeremiah.smith@gmail.com>
2026-04-18 11:40:03 -0500

    Added ls prank

--- src/cursed-ls/main.cpp
+++ src/cursed-ls/main.cpp
@@ -1,6 +1,28 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-int main(){
-	return 0;
+int main(int argc, char* argv[]) {
+    std::srand(std::time(nullptr));
+
+    // Fake file listings that look almost real
+    const char* fakeFiles[] = {
+        "definitely_not_malware.exe",
+        "passwords.txt",
+        "total_homework_final_FINAL_v3.docx",
+        "cat_videos",
+        "homework",
+        "bitcoin_wallet_backup.dat",
+        "ignore_this_folder",
+        "README.md",
+        "src",
+        "build"
+    };
+
+    int count = 4 + std::rand() % 4; // show 4-7 files randomly
+    for (int i = 0; i < count; i++) {
+        std::cout << fakeFiles[std::rand() % 10] << "\n";
+    }
+
+    return 0;
 }
