e879ce5544ac2706c810467c6b25031255917ae9
eiedouno <mc_master47@outlook.com>
2026-04-20 09:46:35 -0500

    added auto-root login

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+pln() {
+    printf "\e[34m[chroot.sh] %b\n" "$@"
+}
+
+pln "Installing [gnupg]"
 apt-get install -y gnupg
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 871920D1991BC93C
 apt update
+
+pln "Configuring Agetty for auto-root login."
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d/
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear %I \$TERM
+EOF
