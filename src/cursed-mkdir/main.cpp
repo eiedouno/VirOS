6dbb6ff5b90689896ab53020b3709dfaba45deae
Grady Smith <166558601+GradyJSmith@users.noreply.github.com>
2026-05-19 13:54:37 -0500

    Added cursed mkdir

--- src/cursed-mkdir/main.cpp
+++ src/cursed-mkdir/main.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib> // for system()
+#include <string>
 
-int main(){
-	return 0;
+int main() {
+	std::string folderName;
+	std::cout << "What is your folder name?: "
+	std::getline(std::cin) >> folderName;
+
+	for(int i = 0; i < 100; i++){
+		std::string command = "mkdir -p " + folderName + i;
+		 // Execute the command
+    	int result = system(command.c_str());
+	}
+   
+
+    if (result == 0) {
+        std::cout << "Directory created successfully.\n";
+    } else {
+        std::cerr << "Failed to create directory.\n";
+    }
+
+    return 0;
 }
